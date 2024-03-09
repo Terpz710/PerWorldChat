e0938b089a857dd0e96cb4502b83452405f6add4
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-03-08 18:57:06 -0800

    Update EventListener.php

--- src/PerWorldChat/EventListener.php
+++ src/PerWorldChat/EventListener.php
@@ -34,7 +34,7 @@ class EventListener implements Listener {
 		    if($this->plugin->cfg["log-chat-disabled"]){
 		        $player->sendMessage(TextFormat::colorize(PerWorldChat::PREFIX . "&cChat is disabled on this world"));
 		    }
-		    $event->setCancelled(true);
+		    $event->cancel();
 		}
 		$recipients = $event->getRecipients();
 		foreach($recipients as $key => $recipient){
